09c7b6129a8c140d81f6e470bf36db09586a9ca5
Alex Bonwit <alexbonwit@mac.com>
2019-11-06 22:13:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
 	    pieces: 4,
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
-	      second: 2,
+	      two_pieces_owned: 2,
 	      third: 3,
 	      fourth: 4
 	    },
